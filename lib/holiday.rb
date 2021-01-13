29482c425d0dad3d092b99ef580558649b8e0a8f
Michael Penn <mwpenn94@gmail.com>
2021-01-13 16:35:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, season_holidays_hash|
+    puts "#{season}:" 
+    season_holidays_hash.each do |attribute, data|
+      puts "#{attribute}: #{data}" 
+      end
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
