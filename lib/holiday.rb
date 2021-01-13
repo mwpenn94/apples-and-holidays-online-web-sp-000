65601c8c2be69f94e4cc494f6d4dbe88153582e9
Michael Penn <mwpenn94@gmail.com>
2021-01-13 17:23:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_holidays_hash|
     puts "#{season.capitalize}:" 
     season_holidays_hash.each do |attribute, data|
-      puts "#{attribute.capitalize}: #{data.join(,)}" 
+      puts "#{attribute.capitalize}: #{data.join}" 
       end
     end
 end
